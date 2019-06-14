821aeedbaa121d6e7ad2f711f8cca91b9d007c60
Gilbert Keen <gilbert.keen@gmail.com>
2019-06-14 12:18:41 +0300

    created a venv and installed django

---
+++ env_myclub/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/KeenPC/Workspaces/django_dev/myclub_site/env_myclub/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
